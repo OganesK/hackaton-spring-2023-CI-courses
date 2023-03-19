085e4a13deb3314911d4513cab9967d0401fdd7d
Mikhail Arkhipov <zet.ten@mail.ru>
2023-03-19 09:43:55 +0300

    change query

--- frontend/src/Pages/Poll/graphql/query.ts
+++ frontend/src/Pages/Poll/graphql/query.ts
@@ -1,13 +1,14 @@
 import { gql } from '@apollo/client';
 
 export const GetTasksQuery = gql`
-    query {
-        tasks {
-            id
-            answers
-            question
-            cost
-            rightAnswer
-        }
+query {
+    tests{
+      name
+      tasks{
+        question
+        answers
+        rightAnswer
+      }
+    }
   }
 `;
